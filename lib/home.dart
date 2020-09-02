95a2ca9577cf6e3048a5e34e5f3fc3dd78460fa5
fanglansheng <qiqi.yan@ideacome.com>
2020-09-02 16:51:49 +0800

    [feature]: 实现再按一次退出

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:wxb/config/env.dart';
 import 'package:wxb/utils/screen_util.dart';
 
@@ -25,32 +26,46 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
+  DateTime lastPopTime;
+
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion(
-      value: SystemUiOverlayStyle.dark,
-      child: WebviewScaffold(
-        enableAppScheme: false,
-        ignoreSSLErrors: true,
-        url: EnvConfig.originUrl,
-        appBar: PreferredSize(
-            child: DecoratedBox(
-              decoration: BoxDecoration(color: Colors.white),
-              child: SizedBox(
-                height: ScreenUtil.statusBarHeight,
-              ),
-            ),
-            preferredSize: Size.fromHeight(ScreenUtil.statusBarHeight)),
-        // withZoom: true,
-        withLocalStorage: true,
-        // hidden: true,
-        // initialChild: Container(
-        //   color: Colors.redAccent,
-        //   child: const Center(
-        //     child: Text('Waiting.....'),
-        //   ),
-        // ),
-      ),
-    );
+    return WillPopScope(
+        child: AnnotatedRegion(
+          value: SystemUiOverlayStyle.dark,
+          child: WebviewScaffold(
+            enableAppScheme: false,
+            ignoreSSLErrors: true,
+            url: EnvConfig.originUrl,
+            appBar: PreferredSize(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: SizedBox(
+                    height: ScreenUtil.statusBarHeight,
+                  ),
+                ),
+                preferredSize: Size.fromHeight(ScreenUtil.statusBarHeight)),
+            // withZoom: true,
+            withLocalStorage: true,
+            // hidden: true,
+            // initialChild: Container(
+            //   color: Colors.redAccent,
+            //   child: const Center(
+            //     child: Text('Waiting.....'),
+            //   ),
+            // ),
+          ),
+        ),
+        onWillPop: () async {
+          if (lastPopTime == null || DateTime.now().difference(lastPopTime) > Duration(seconds: 2)) {
+            lastPopTime = DateTime.now();
+            Fluttertoast.showToast(msg: '再按一次退出');
+            return false;
+          } else {
+            lastPopTime = DateTime.now();
+            await SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+            return true;
+          }
+        });
   }
 }
